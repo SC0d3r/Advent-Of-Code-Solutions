ba7152b4664be039a3617340bcd2ddcd89ddf69b
SC0d3r <sasan.kelishani@gmail.com>
2025-04-11 11:44:19 +0330

    optimize

--- 2024/day5/part2/part2.go
+++ 2024/day5/part2/part2.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"io"
+	"bufio"
 	"log"
-	"math"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
-
-type Rule struct {
-	Before int
-}
 
 func main() {
 	file, err := os.Open("./2024/day5/inp1.txt")
@@ -20,181 +16,163 @@ func main() {
 	}
 	defer file.Close()
 
-	rules := make(map[string]bool) // "X|Y":true
-	updates := make([][]int, 0)
+	scanner := bufio.NewScanner(file)
+	// rules[x][y]==true means x should come before y
+	rules := make(map[int]map[int]bool)
+	updates := [][]int{}
 
-	data, err := io.ReadAll(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lines := strings.Split(string(data), "\n")
-	for i, line := range lines {
-		line = strings.TrimSpace(line)
+	// Read rules until a blank line is encountered
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
 		if line == "" {
-			// here we are processing updates
-			updates = getUpdates(lines[i+1:])
 			break
 		}
-		// process rules
 		addRule(line, rules)
 	}
 
-	log.Println("rules", rules)
-	log.Println("updates", updates)
-
-	// a := []int{61, 13, 29}
-	// a := []int{75, 47, 61, 53, 29}
-	// for i := 0; i < len(a)-1; i++ {
-	// 	v := a[i]
-	// 	if !applyRules(v, a[i+1:], rules) {
-	// 		log.Println("wrong update")
-	// 	}
-	// }
-
-	wUpdates := make([][]int, 0)
-
-	for _, update := range updates {
-		correct := true
-		for i := 0; i < len(update)-1; i++ {
-			v := update[i]
-			if ap, _ := applyRules(v, update[i+1:], rules); !ap {
-				log.Println("wrong update", update)
-				correct = false
-				break
-			}
-		}
-
-		if !correct {
-			wUpdates = append(wUpdates, update)
-		}
-	}
-
-	// make them in correct order
-	for _, update := range wUpdates {
-		for {
-			changeHappend := false
-			for i := 0; i < len(update)-1; i++ {
-				v := update[i]
-				ap, key := applyRules(v, update[i+1:], rules)
-				if !ap {
-					// rules doesnt apply
-					changeHappend = true
-					reverse(key, update)
-				}
-			}
-
-			// otherwise we have to check the update as long
-			// as we can apply changes cause each new change
-			// we have to apply the rules again
-			if !changeHappend {
-				break
-			}
-		}
-	}
-
-	// get the mids
-	mids := make([]int, 0)
-	for _, cs := range wUpdates {
-		idx := math.Floor(float64(len(cs) / 2))
-		mids = append(mids, cs[int(idx)])
-	}
-
-	sum := 0
-	for _, x := range mids {
-		sum += x
-	}
-
-	log.Println("wrong updates", wUpdates)
-	log.Println("mids", mids, "sum", sum)
-}
-
-func applyRules(X int, Ys []int, rules map[string]bool) (bool, string) {
-	for _, Y := range Ys {
-		key := strconv.Itoa(X) + "|" + strconv.Itoa(Y)
-		val, ok := rules[key]
-		log.Println("key is", key, "val is", val, "ok is", ok)
-		if !ok {
+	// Read update lines after the blank line
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
 			continue
 		}
+		update, err := parseUpdate(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		updates = append(updates, update)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-		// here the rule exists
-		if !val {
-			// invalid order
-			return false, key
+	start := time.Now()
+
+	// Identify wrong updates (updates that don't follow the rules)
+	var wrongUpdates [][]int
+	for _, update := range updates {
+		if !isUpdateCorrect(update, rules) {
+			wrongUpdates = append(wrongUpdates, update)
 		}
 	}
 
-	return true, ""
-}
-
-func addRule(line string, rules map[string]bool) {
-	xs := strings.Split(strings.TrimSpace(line), "|")
-
-	// X|Y set to true
-	key1 := xs[0] + "|" + xs[1]
-	rules[key1] = true
-
-	// Y|X set to false meaning that Y should not come before X
-	// but what if it
-	key2 := xs[1] + "|" + xs[0]
-
-	val, ok := rules[key2]
-	if ok && val {
-		// if it exists and value is true then this is a contradiction
-		// so both X wants to be before Y and Y wants to be before X
-		// is this allowed?
-		log.Fatal("contradiction", "key2", key2, "key1", key1)
-	}
-
-	rules[key2] = false
-}
-
-func getUpdates(lines []string) [][]int {
-	res := make([][]int, 0)
-	for _, line := range lines {
-		xs := strings.Split(strings.TrimSpace(line), ",")
-		ret := make([]int, 0)
-		for _, v := range xs {
-			// log.Println("v is", v, "xs is", xs, "len", len(xs))
-			vi, err := strconv.Atoi(v)
-			if err != nil {
-				log.Fatal(err)
+	// Fix wrong updates by swapping conflicting elements until no changes occur
+	for _, update := range wrongUpdates {
+		for {
+			changed := false
+			for i := 0; i < len(update)-1; i++ {
+				if valid, pair := checkRule(update[i], update[i+1:], rules); !valid {
+					swap(update, pair[0], pair[1])
+					changed = true
+				}
 			}
-			ret = append(ret, vi)
+			if !changed {
+				break
+			}
 		}
-		res = append(res, ret)
 	}
 
-	return res
+	// Compute the middle element from each wrong update and sum them up
+	mids := make([]int, 0, len(wrongUpdates))
+	sum := 0
+	for _, upd := range wrongUpdates {
+		mid := upd[len(upd)/2] // using integer division for the mid index
+		mids = append(mids, mid)
+		sum += mid
+	}
+
+	log.Println("wrong updates", wrongUpdates)
+	log.Println("mids", mids, "sum", sum)
+	log.Println("took", time.Since(start))
 }
 
-func reverse(key string, update []int) {
-	nums := strings.Split(key, "|")
-	X, err := strconv.Atoi(nums[0])
+// addRule adds a rule from a line "X|Y", meaning X should come before Y.
+// It also sets the inverse rule (Y|X) to false.
+func addRule(line string, rules map[int]map[int]bool) {
+	parts := strings.Split(strings.TrimSpace(line), "|")
+	if len(parts) != 2 {
+		log.Fatal("invalid rule:", line)
+	}
+	x, err := strconv.Atoi(parts[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	y, err := strconv.Atoi(parts[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	Y, err := strconv.Atoi(nums[1])
-	if err != nil {
-		log.Fatal(err)
+	// Set rule: x should be before y.
+	if rules[x] == nil {
+		rules[x] = make(map[int]bool)
 	}
+	rules[x][y] = true
 
-	// we are here cause X happend before Y and that was an erro
-	// so here we have to put the Y before X in update
-	Xidx := 0
-	Yidx := 0
+	// Set inverse rule: y should not come before x.
+	if rules[y] == nil {
+		rules[y] = make(map[int]bool)
+	}
+	if valid, exists := rules[y][x]; exists && valid {
+		log.Fatalf("contradiction: rule %d|%d conflicts with %d|%d", y, x, x, y)
+	}
+	rules[y][x] = false
+}
+
+// parseUpdate converts a comma-separated line into a slice of integers.
+func parseUpdate(line string) ([]int, error) {
+	parts := strings.Split(line, ",")
+	result := make([]int, len(parts))
+	for i, p := range parts {
+		p = strings.TrimSpace(p)
+		num, err := strconv.Atoi(p)
+		if err != nil {
+			return nil, err
+		}
+		result[i] = num
+	}
+	return result, nil
+}
+
+// isUpdateCorrect checks whether an update order follows all the rules.
+func isUpdateCorrect(update []int, rules map[int]map[int]bool) bool {
+	for i := 0; i < len(update)-1; i++ {
+		if valid, _ := checkRule(update[i], update[i+1:], rules); !valid {
+			return false
+		}
+	}
+	return true
+}
+
+// checkRule verifies that for element X with following elements Ys the rule holds.
+// It returns false and the pair [X, Y] if a rule violation is found.
+func checkRule(X int, Ys []int, rules map[int]map[int]bool) (bool, []int) {
+	if ruleMap, exists := rules[X]; exists {
+		for _, Y := range Ys {
+			if valid, ok := ruleMap[Y]; ok {
+				if !valid {
+					return false, []int{X, Y}
+				}
+			}
+		}
+	}
+	return true, nil
+}
+
+// swap exchanges the positions of x and y in the update slice.
+func swap(update []int, x, y int) {
+	idxX, idxY := -1, -1
 	for i, v := range update {
-		if v == X {
-			Xidx = i
+		if v == x && idxX == -1 {
+			idxX = i
 		}
-
-		if v == Y {
-			Yidx = i
+		if v == y && idxY == -1 {
+			idxY = i
+		}
+		if idxX != -1 && idxY != -1 {
+			break
 		}
 	}
-
-	// reverse
-	update[Xidx], update[Yidx] = update[Yidx], update[Xidx]
+	if idxX != -1 && idxY != -1 {
+		update[idxX], update[idxY] = update[idxY], update[idxX]
+	}
 }
