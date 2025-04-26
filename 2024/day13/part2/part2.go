d249dc65a741a4db12630ddbbd3358cec126732f
SC0d3r <sasan.kelishani@gmail.com>
2025-04-27 01:24:24 +0330

    solve part 2 day13 2024

--- 2024/day13/part2/part2.go
+++ 2024/day13/part2/part2.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"log"
-	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -18,13 +17,14 @@ type Partial struct {
 
 type LB string
 
-const lbA LB = "A"
-const lbB LB = "B"
+const (
+	lbA LB = "A"
+	lbB LB = "B"
+)
 
 type Btn struct {
-	l LB
-	x int
-	y int
+	l    LB
+	x, y int
 }
 
 type PZ struct {
@@ -32,19 +32,12 @@ type PZ struct {
 }
 
 type Press struct {
-	btnA int
-	btnB int
-}
-
-type BtnPress struct {
-	btn Btn
-	cnt int // count
+	btnA, btnB int
 }
 
 type MN struct {
-	a  Btn
-	b  Btn
-	pz PZ
+	a, b Btn
+	pz   PZ
 }
 
 func main() {
@@ -54,201 +47,135 @@ func main() {
 	}
 	defer file.Close()
 
-	mns := []MN{}
-
+	var mns []MN
 	sc := bufio.NewScanner(file)
 	for sc.Scan() {
+		// each block is 4 lines: A, B, target, blank
 		mns = append(mns, createMN(sc))
 	}
+	if err := sc.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-	log.Println("mns", mns)
-
-	sumt := 0
-
-	// fmc := mns[0]
-	// press := newAlgo(fmc.a, fmc.b, fmc.pz)
-	// log.Println("press", press)
-
+	sumTokens := 0
 	for _, mn := range mns {
 		pressA := newAlgo(mn.a, mn.b, mn.pz)
-		// 	// reverse the btn
-		pressB := algo(mn.b, mn.a, mn.pz)
+		pressB := newAlgo(mn.b, mn.a, mn.pz)
 
-		if pressA.btnA == -1 || pressB.btnA == -1 {
+		if pressA.btnA < 0 || pressB.btnA < 0 {
 			continue
 		}
 
-		ta := (pressA.btnA * 3) + (pressA.btnB * 1)
-		t := ta
-
+		ta := pressA.btnA*3 + pressA.btnB*1
+		tb := pressB.btnA*1 + pressB.btnB*3
+		best := ta
 		if pressA.btnA != pressB.btnA && pressA.btnB != pressB.btnB {
-			// if different
-			tb := (pressB.btnA * 1) + (pressB.btnB * 3)
-			t = min(ta, tb)
+			if tb < ta {
+				best = tb
+			}
 		}
-
-		log.Println("pressA", pressA, "pressB", pressB, "ta", ta, "t", t)
-
-		sumt += t
+		sumTokens += best
 	}
 
-	log.Println("sum token", sumt)
+	log.Println("sum tokens:", sumTokens)
 }
 
 func createMN(sc *bufio.Scanner) MN {
-	l := strings.TrimSpace(sc.Text())
-	if l == "" {
-		log.Fatal("this should not happen")
-	}
-	btna := readBtn(l, lbA)
-
-	// advance the scanner
-	if !sc.Scan() {
-		log.Fatal("shouldnt happen")
-	}
-
-	l = strings.TrimSpace(sc.Text())
-	if l == "" {
-		log.Fatal("this should not happen")
-	}
-	btnb := readBtn(l, lbB)
-
-	// advance the scanner
-	if !sc.Scan() {
-		log.Fatal("shouldnt happen")
-	}
-
-	l = strings.TrimSpace(sc.Text())
-	if l == "" {
-		log.Fatal("this should not happen")
-	}
-	pz := readPz(l)
-
-	// advance the scanner
+	// line A
+	a := strings.TrimSpace(sc.Text())
+	btnA := readBtn(a, lbA)
+	// line B
 	sc.Scan()
-
-	return MN{a: btna, b: btnb, pz: pz}
+	btnB := readBtn(strings.TrimSpace(sc.Text()), lbB)
+	// line target
+	sc.Scan()
+	pz := readPz(strings.TrimSpace(sc.Text()))
+	// blank (or EOF)
+	sc.Scan()
+	return MN{a: btnA, b: btnB, pz: pz}
 }
 
-func readPz(l string) PZ {
-	re := regexp.MustCompile(`\=(\d+)`)
-
-	// btn a
-	res := re.FindAllString(l, -1)
-	xstr := res[0][1:] // skipping +
-	ystr := res[1][1:]
-	x, err := strconv.Atoi(xstr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	y, err := strconv.Atoi(ystr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// return PZ{x: x + 10000000000000, y: y + 10000000000000}
-	return PZ{x: x, y: y}
-}
-
-func readBtn(l string, label LB) Btn {
+func readBtn(line string, label LB) Btn {
 	re := regexp.MustCompile(`\+(\d+)`)
-
-	// btn a
-	res := re.FindAllString(l, -1)
-	xstr := res[0][1:] // skipping +
-	ystr := res[1][1:]
-	x, err := strconv.Atoi(xstr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	y, err := strconv.Atoi(ystr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	fields := re.FindAllStringSubmatch(line, -1)
+	x, _ := strconv.Atoi(fields[0][1])
+	y, _ := strconv.Atoi(fields[1][1])
 	return Btn{l: label, x: x, y: y}
 }
 
-func gcd(x int, y int) Partial {
-	r0 := x
-	r1 := y
-	u0 := 1
-	u1 := 0
-	v0 := 0
-	v1 := 1
+func readPz(line string) PZ {
+	re := regexp.MustCompile(`=(\d+)`)
+	fields := re.FindAllStringSubmatch(line, -1)
+	x, _ := strconv.Atoi(fields[0][1])
+	y, _ := strconv.Atoi(fields[1][1])
+	return PZ{x: x + 10000000000000, y: y + 10000000000000}
+}
 
-	for r1 != 0 {
-		q := int(math.Floor(float64(r0 / r1)))
-		r0, r1 = r1, r0-q*r1
+// Extended Euclidean: returns gcd and one pair (u,v) such that u*x + v*y = gcd.
+func gcd(x, y int) Partial {
+	u0, u1 := 1, 0
+	v0, v1 := 0, 1
+	for y != 0 {
+		q := x / y
+		x, y = y, x-q*y
 		u0, u1 = u1, u0-q*u1
 		v0, v1 = v1, v0-q*v1
 	}
-
-	return Partial{
-		gcd: r0,
-		u:   u0,
-		v:   v0,
-	}
+	return Partial{gcd: x, u: u0, v: v0}
 }
 
-func newAlgo(a Btn, b Btn, pz PZ) Press {
-	// for x
+// Solve simultaneously:
+//
+//	m*a.x + n*b.x = pz.x
+//	m*a.y + n*b.y = pz.y
+//
+// Return (-1,-1) if no non-negative integer solution.
+func newAlgo(a, b Btn, pz PZ) Press {
+	// 1) Check solvability of each axis
 	gx := gcd(a.x, b.x)
 	if pz.x%gx.gcd != 0 {
-		// no solution
-		return Press{btnA: -1, btnB: -1}
+		return Press{-1, -1}
 	}
-
-	// for y
 	gy := gcd(a.y, b.y)
 	if pz.y%gy.gcd != 0 {
-		// no solution
-		return Press{btnA: -1, btnB: -1}
+		return Press{-1, -1}
 	}
 
-	// for x
+	// 2) One particular solution for the X-equation:
+	//    m0 = u*(pz.x/g)
+	//    n0 = v*(pz.x/g)
 	m0 := gx.u * (pz.x / gx.gcd)
 	n0 := gx.v * (pz.x / gx.gcd)
 
-	t := 0
-	// find the first t in which mx and n are both positive
-	mx := m0 + ((b.x / gx.gcd) * -t) // 1 = t
-	nx := n0 - ((a.x / gx.gcd) * -t) // 1 = t
+	// 3) We know the general X-soln is
+	//    m = m0 + k*(b.x/gx)
+	//    n = n0 - k*(a.x/gx)
+	// Plug into Y:
+	//   (m0 + k*(b.x/gx))*a.y + (n0 - k*(a.x/gx))*b.y = pz.y
+	// ⇒ k * [ (b.x/gx)*a.y - (a.x/gx)*b.y ] = pz.y - (m0*a.y + n0*b.y)
+	det := (b.x/gx.gcd)*a.y - (a.x/gx.gcd)*b.y
+	rhs := pz.y - (m0*a.y + n0*b.y)
 
-	prevMx := mx
-	prevNx := nx
-	sign := 1
-	noPath := 1
-	for mx < 0 || nx < 0 {
-		if noPath >= 3 {
-			log.Println(":: NO SOLUTION", "a", a, "b", b, "mx", mx, "nx", nx)
+	switch {
+	case det == 0:
+		// either no solution (rhs≠0) or infinite (rhs=0).
+		if rhs != 0 {
 			return Press{-1, -1}
 		}
-
-		t += sign
-		mx = m0 + ((b.x / gx.gcd) * -t)
-		nx = n0 - ((a.x / gx.gcd) * -t)
-
-		if mx < 0 && mx < prevMx {
-			// becomming even more negative
-			noPath++
-			sign *= -1
+		// infinite many—pick the one that is non-negative if m0,n0 already ≥0
+		if m0 >= 0 && n0 >= 0 {
+			return Press{m0, n0}
 		}
-
-		if nx < 0 && nx < prevNx {
-			// becomming even more negative
-			noPath++
-			sign *= -1
-		}
-
-		// if t > 1e6 {
-		// 	return Press{-1, -1}
-		// }
+		return Press{-1, -1}
+	case rhs%det != 0:
+		return Press{-1, -1}
 	}
 
-	valx := mx*a.x + nx*b.x
-
-	log.Println("a", a, "b", b, "pz", pz, "valx", valx, "mx", mx, "nx", nx)
-
-	return Press{btnA: mx, btnB: nx}
+	k := rhs / det
+	m := m0 + k*(b.x/gx.gcd)
+	n := n0 - k*(a.x/gx.gcd)
+	if m < 0 || n < 0 {
+		return Press{-1, -1}
+	}
+	return Press{m, n}
 }
