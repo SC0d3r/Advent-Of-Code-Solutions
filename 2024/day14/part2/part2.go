3dd5eba7d24d2011f1344300d35ac91bf7fc3b32
SC0d3r <sasan.kelishani@gmail.com>
2025-04-27 19:46:09 +0330

    solve part 2 day14 2024

--- 2024/day14/part2/part2.go
+++ 2024/day14/part2/part2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -45,74 +46,55 @@ func main() {
 
 	log.Println("len", len(bots))
 
-	// rows := 7
-	// cols := 11
-
 	rows := 103
 	cols := 101
 
-	// fb := Bot{p: Pos{x: 2, y: 4}, v: Vel{x: 2, y: -3}}
-	// log.Println("fb before", fb)
-	// step(&fb, rows, cols)
-	// step(&fb, rows, cols)
-	// step(&fb, rows, cols)
-	// step(&fb, rows, cols)
-	// step(&fb, rows, cols)
-	// log.Println("fb after", fb)
-
-	// for i := 0; i < 100; i++ {
-	// 	for _, b := range bots {
-	// 		step(b, rows, cols)
-	// 	}
-	// }
-
-	// log.Println("bots after step", bots)
-
-	// b := Bot{p: Pos{x: cols - 1, y: rows - 1}, v: Vel{}}
-	// log.Println("inQuad", "bot", b, inQuad(b, rows, cols))
-	// tree := tposes(len(bots), rows, cols)
-	// log.Println("tree", tree)
-
-	// for !isTree(bots, tree) {
-	// 	for _, b := range bots {
-	// 		// log.Println("steping")
-	// 		step(b, rows, cols)
-	// 	}
-	// }
-
-	// best height
-	bh := int(1e12)
-	// best t
-	bt := 0
-
-	for t := 0; t < 200000; t++ {
+	for t := range 200000 {
 		for _, b := range bots {
 			step(b, rows, cols)
 		}
 
-		// after each  step
-		minY := int(1e9)
-		maxY := 0
+		// no overlaping
+		ovlp := false
+		m := map[Pos]struct{}{}
 		for _, b := range bots {
-			if b.p.y < (minY) {
-				minY = b.p.y
+			_, exist := m[b.p]
+			if exist {
+				ovlp = true
+				break
 			}
 
-			if b.p.y > maxY {
-				maxY = b.p.y
-			}
+			m[b.p] = struct{}{}
 		}
-
-		h := maxY - minY + 1
-		if h < bh {
-			bh = h
-			bt = t
+		if !ovlp {
+			// no overlaping
+			plotBots(bots, rows, cols)
+			log.Fatal(":: Found:" + " t:" + strconv.Itoa(t+1))
 		}
 	}
 
-	mx := cols / 2
-	my := rows / 2
-	log.Println("mx", mx, "my", my, "bh", bh, "bt", bt)
+	plotBots(bots, rows, cols)
+}
+
+func plotBots(bots []*Bot, rows, cols int) {
+	// Create an empty grid
+	grid := make([][]rune, rows)
+	for y := range grid {
+		grid[y] = make([]rune, cols)
+		for x := range grid[y] {
+			grid[y][x] = '.'
+		}
+	}
+
+	// Place bots
+	for _, b := range bots {
+		grid[b.p.y][b.p.x] = '#'
+	}
+
+	// Print grid
+	for _, row := range grid {
+		fmt.Println(string(row))
+	}
 }
 
 func step(b *Bot, rows int, cols int) {
@@ -140,101 +122,4 @@ func createBot(l string) Bot {
 	vel := Vel{x: v1, y: v2}
 
 	return Bot{p: pos, v: vel}
-}
-
-func fibo(n int) int {
-	l := []int{1, 1}
-	for ; n > 0; n-- {
-		l[0], l[1] = l[1], l[0]+l[1]
-	}
-	return l[1]
-}
-
-func tposes(lenBots int, rows int, cols int) map[int][]Pos {
-	mx := cols / 2
-	// my := rows / 2
-
-	res := map[int][]Pos{}
-	res[0] = []Pos{Pos{mx, 0}}
-
-	for i := 0; i < rows; i++ {
-		if lenBots == 0 {
-			// no more bots left
-			break
-		}
-
-		// pposes := res[len(res)-1]
-		pposes := res[i]
-
-		// gen next poses
-		nposes := []Pos{}
-		l := len(pposes) / 2
-		// for i, pp := range pposes {
-		pp := pposes[l] // the middle one
-		for j := -(pp.y + 1); j <= pp.y+1; j++ {
-			nposes = append(nposes, Pos{x: pp.x + j, y: pp.y + 1})
-			lenBots--
-		}
-
-		res[i+1] = nposes
-	}
-
-	return res
-}
-
-func isTree(bots []*Bot, tree map[int][]Pos) bool {
-	//     *
-	//   * * *
-	// * * * * *
-
-	// so maybe
-	// create all the poses for valid tree [][]Pos like {{{3,0}}, {{3,5},{8,5}}}
-	// create map of bots based on theirs pos.y
-	// for each row of valid poses:
-	// 1. threre should be enough bots
-	// 2. bots should be in specified cols (x)
-
-	// used bots
-	used := 0
-	usedBs := []Bot{}
-
-	// check each row of tree
-	for /*row*/ _, trow := range tree {
-		rowSatisfied := len(trow)
-		for _, pos := range trow {
-			// find a robot which satisfies this
-			// if no bot found then return false
-			fnd := false
-			for _, b := range bots {
-				if b.p.x == pos.x && b.p.y == pos.y {
-					fnd = true
-					used++
-					rowSatisfied--
-					usedBs = append(usedBs, *b)
-					// should i break?
-					// cause of overlaping bots
-					// i have to check and use all bots right?
-					break
-				}
-			}
-			if !fnd {
-				return false
-			}
-			// log.Println("trow satisfied", trow, "bots", usedBs)
-			// log.Println("rowSatisfied", rowSatisfied)
-		}
-
-		if rowSatisfied > 0 {
-			log.Println("didnd row satisfied")
-			return false
-		}
-	}
-
-	remBots := len(bots) - used
-	log.Println("rembots", remBots, "used", used)
-	if remBots > 0 {
-		// still bots which are remaining
-	}
-
-	return true
 }
